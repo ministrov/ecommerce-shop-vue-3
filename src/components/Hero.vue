ad4ad348bdbbbdf07c7f1bf3b3a1733557907465
ministrov <61511030+ministrov@users.noreply.github.com>
2023-02-03 10:13:46 +0300

    feat: finish Header component and start Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero">
+    <h2 class="visually-hidden">Hero section</h2>
+    <img class="hero-img" src="/img/hero-bg.jpg" width="1440" height="704" alt="hero-bg">
+    <div class="hero-info">
+      <h3>Luxury homeware for people who love timeless design quality</h3>
+      <p>Shop the new Spring 2022 collection today</p>
+      <a>View collection</a>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  &-img {
+    max-width: 100%;
+  }
+}
+</style>
